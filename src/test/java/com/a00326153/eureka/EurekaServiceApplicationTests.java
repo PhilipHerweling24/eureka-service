3c6f89a291c5848d1daf20b283fff61492db12ac
PhilipHerweling <miggymorette@gmail.com>
2025-04-06 13:54:48 +0100

    Initialised project

---
+++ src/test/java/com/a00326153/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a00326153.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
